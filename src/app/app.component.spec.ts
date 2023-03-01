fa700553f99aab253fa3a57d136e79e782f029f9
annamelnyk <anna_melnyk@epam.com>
2023-03-02 00:13:47 +1100

    renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'elenium-task'`, () => {
+  it(`should have as title 'elen-task'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('elenium-task');
+    expect(app.title).toEqual('elen-task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('elenium-task app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('elen-task app is running!');
   });
 });
